a335ecf4bb2bb2794c4a160947c883573e92b422
Jb <joaofilipe.barbosa6@gmail.com>
2026-02-13 23:50:37 -0300

    Started the script for games need to finish the axios request

--- frontend/scripts/games.js
+++ frontend/scripts/games.js
@@ -6,3 +6,33 @@ const api = axios.create(
 
 
 const form = document.getElementById('gameForm')
+const error_div = document.getElementById('error-div')
+const checkboxes = document.querySelectorAll('input[name="games"]');
+
+
+function clearError(){
+    error_div.style.display = 'none'
+    error_div.innerText = ''
+}
+checkboxes.forEach(box=>{
+    box.addEventListener('change',()=>{
+        clearError()
+    })
+})
+form.addEventListener('submit',async(e)=>{
+    e.preventDefault()
+
+    const selectedCheckboxes = document.querySelectorAll('input[name="games"]:checked');
+
+    const selectedGames = Array.from(selectedCheckboxes).map(checkbox=>{
+        checkbox.value
+    })
+
+    if(selectedGames.length===0){
+        error_div.style.display = "block"
+        error_div.innerText = "Select at least one game!"
+        return
+    }
+
+    
+})
